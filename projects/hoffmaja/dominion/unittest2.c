24ce3e3a77f21c4ee841e1c9c6377b62abe7767a
Ja5onHoffman <j@sonhoffman.com>
2017-07-12 13:42:26 -0500

    Unit test 2 complete

--- projects/hoffmaja/dominion/unittest2.c
+++ projects/hoffmaja/dominion/unittest2.c
@@ -12,9 +12,9 @@
 Unit test for gainCard function.
 Add card to a players hand then check for card.
 *********/
-
+int errs = 0;
 #undef assert
-#define assert(cond) { if (!(cond)) { printf("--FAILED TEST--\n"); } else { printf("**PASS**\n"); }}
+#define assert(cond) { if (!(cond)) { printf("--FAILED TEST--\n"); errs++; } else { printf("**PASS**\n"); }}
 
 int main() {
   struct gameState state;
@@ -28,7 +28,7 @@ int main() {
   // Initialize game
   initializeGame(numPlayers, k, seed, &state);
 
-  printf("Checking 'add to deck' flag\n");
+  printf("***** Checking 'add to deck' flag ********\n\n");
   printf("Deck count: %d\n", state.deckCount[0]);
   printf("Hand count: %d\n", state.handCount[0]);
   printf("Discard count: %d\n\n", state.discardCount[0]);
@@ -52,13 +52,10 @@ int main() {
   printf("Hand should contain adventureer at position %d\n", state.deckCount[0]-1);
   assert(state.deck[0][state.deckCount[0]-1] == adventurer);
 
-  // printf("Deck should contain adventurer");
-  // assert(state.supply)
-
-
   count = 0; // Reset count
+  supp  = 0; // Reset supply
 
-  printf("\nChecking 'add to discard' flag\n");
+  printf("\n********* Checking 'add to discard' flag *********\n\n");
 
   printf("Deck count: %d\n", state.deckCount[0]);
   printf("Hand count: %d\n", state.handCount[0]);
@@ -67,6 +64,7 @@ int main() {
   printf("Discard before discarding card:\n");
   printDiscard(0, &state);
   count = state.discardCount[0]; // Set original discard count
+  supp = supplyCount(adventurer, &state);
   gainCard(adventurer, &state, 0, 0);
   printf("Deck count: %d\n", state.deckCount[0]);
   printf("Hand count: %d\n", state.handCount[0]);
@@ -76,10 +74,53 @@ int main() {
 
   printf("Discard count should be %d\n", count+1);
   assert(state.discardCount[0] == count+1);
+  printf("Supply count should be %d\n", supp-1);
+  assert(supplyCount(adventurer, &state) == supp-1);
+  printf("Discard should contain adventurer at position %d\n", state.discardCount[0]-1);
+  assert(state.discard[0][state.discardCount[0]-1] == adventurer);
 
   count = 0; // Reset count
+  supp  = 0;  // Reset supply
 
+  printf("\n********* Checking 'add to hand' flag *********\n\n");
 
+  printf("Deck count: %d\n", state.deckCount[0]);
+  printf("Hand count: %d\n", state.handCount[0]);
+  printf("Discard count: %d\n\n", state.discardCount[0]);
+  printf("Hand before adding card:\n");
+  printHand(0, &state);
+  count = state.handCount[0]; // Set original hand count
+  supp = supplyCount(adventurer, &state);;
+  printf("\nAdding adventurer card...\n");
+  gainCard(adventurer, &state, 2, 0); // Add card
+  printf("Deck count: %d\n", state.deckCount[0]);
+  printf("Hand count: %d\n", state.handCount[0]);
+  printf("Discard count: %d\n\n", state.discardCount[0]);
+
+  printf("Hand after adding card:\n");
+  printHand(0, &state); // Show hand with new card
+
+  printf("Hand count should be %d\n", count+1);
+  assert(state.handCount[0] == count+1); // Verify hand increase by one card
+  printf("Supply of card should be %d\n", supp-1);
+  assert(supplyCount(adventurer, &state) == supp-1);
+  printf("Hand should contain adventurer at position %d\n", state.handCount[0]-1);
+  assert(state.hand[0][state.handCount[0]-1] == adventurer);
+
+  count = 0; // Reset count
+  supp  = 0; // Reset supply
+
+  printf("\n******** Checking for non-card ********\n\n");
+
+  printf("Adding nonexistent card....\n");
+  int noncard = -1;
+  int err = gainCard(noncard, &state, 0, 0);
+
+  printf("Function should return -1\n");
+  assert(err == -1);
+
+  printf("\n\n\n");
+  printf("Total failed tests: %d\n\n", errs);
 
 
 
